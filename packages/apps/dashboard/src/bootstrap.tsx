b6ded054b46366e173fabc85fb221e41c109e38a
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-11-09 23:50:51 +0300

    remove temp dashboard

--- packages/apps/dashboard/src/bootstrap.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ProjectRoute from './route';
-
-ReactDOM.render(<ProjectRoute />, document.getElementById('root'));
--- packages/apps/dashboard/src/route.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const DashboardRoute = () => {
-  return <p>Dashboard route</p>;
-};
-
-export default DashboardRoute;
